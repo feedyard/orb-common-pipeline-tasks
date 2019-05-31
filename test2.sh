46ffef22cb9a9403735c2c84831a7d6972f58ace
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-05-31 15:46:39 -0500

    iterating on orb-tools capabilities

--- test2.sh
+++ test2.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-CIRCLE_COMPARE_URL=$(cat CIRCLE_COMPARE_URL.txt)
+CIRCLE_COMPARE_URL="https://github.com/feedyard/orb-common-pipeline-tasks/compare/56bcf095fb29...8bf617096b94"
 
 COMMIT_RANGE=$(echo $CIRCLE_COMPARE_URL | sed 's:^.*/compare/::g')
 echo "Commit range: $COMMIT_RANGE"
 
 # only publish a major release if there are new jobs or commands
-if [[ $(git diff $COMMIT_RANGE --name-status | grep -e "A      src/commands" -e "A      src/jobs") ]]; then
+if ( git diff $COMMIT_RANGE --name-status | grep -e "A      src/commands" -e "A      src/jobs" ); then
 export INTEGRATION_TAG=major
 # publish a minor release if there are other changes to jobs or commands
-elif [[ $(git diff $COMMIT_RANGE --name-status | grep -e "src/commands" -e "src/jobs") ]]; then
+elif ( git diff $COMMIT_RANGE --name-status | grep -e "src/commands" -e "src/jobs" ); then
 export INTEGRATION_TAG=minor
 # patch release if any changers to examples, executors, @orb.yml
-elif [[ $(git diff $COMMIT_RANGE --name-status | grep -e "src/examples" -e "src/executors" -e "src/@orb.yml") ]]; then
+elif ( git diff $COMMIT_RANGE --name-status | grep -e "src/examples" -e "src/executors" -e "src/@orb.yml" ); then
 export INTEGRATION_TAG=patch
 # otherwise, don't publish a release
 else
